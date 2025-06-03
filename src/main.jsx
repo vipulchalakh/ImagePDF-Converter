f8775487037f4d6b28a002d1dcad4434639c0b73
Vipul Chalakh <vipulchalakh9@gmail.com>
2025-06-04 01:09:11 +0530

    Update: added vercel insights.

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
 import { HelmetProvider } from 'react-helmet-async';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
+      <SpeedInsights />
     </HelmetProvider>
   </React.StrictMode>
 );
